1cb5ccb7ddcb3467a0bdf368979151d17259374c
Tiny-Fire <15501005105@163.com>
2018-09-13 09:37:11 +0800

    微信登录

---
+++ zhaotest/indexphone.php
@@ -0,0 +1,23 @@
+<?php
+	include('./wechatlogin/1.php');
+
+	define('APPID',            "bgfdsaf");  //此处是公众号的appid及appsecret
+	define('APPSECRET',        "mjhfgfb");
+	define('REDIRECT_URI',     urlEncode('http://333.2222.cn/website/zhaotest/indexphone.php'));
+	define('SCOPE',            "snsapi_userinfo");
+	
+	$weixin = new class_app();
+	//传入授权临时票据（code）
+	$code = $_GET['code'];
+	if(!$code){
+		$code = $weixin->getcodephone();
+	}
+	// var_dump($code);die;
+	
+	$oauth2_info = $weixin->oauth2_access_token($code);
+	
+	$result = $weixin->oauth2_get_user_info($oauth2_info['access_token'], $oauth2_info['openid']);
+	echo '<pre/>';
+	var_dump($result);
+
+?>
